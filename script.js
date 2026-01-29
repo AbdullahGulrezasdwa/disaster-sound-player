e9a5d217d05e9d0e08552659ffc26177920af470
AbdullahGulrezasdwa <abdullahgulrez123@gmail.com>
2026-01-29 16:33:41 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
-const wildfire = new Audio("wildfire.mp3");
-const hurricane = new Audio("hurricane.mp3");
+const wildfire = new Audio('wildfire.mp3');
+const hurricane = new Audio('hurricane.mp3');
 
-const wildfireBtn = document.getElementById("wildfireBtn");
-const hurricaneBtn = document.getElementById("hurricaneBtn");
+const wildfireBtn = document.getElementById('wildfireBtn');
+const hurricaneBtn = document.getElementById('hurricaneBtn');
+const stopBtn = document.getElementById('stopBtn');
+const volumeSlider = document.getElementById('volumeSlider');
 
-function stopAll() {
-  wildfire.pause();
-  hurricane.pause();
+let currentAudio = null;
+let currentButton = null;
 
-  wildfire.currentTime = 0;
-  hurricane.currentTime = 0;
+// Set default volume
+wildfire.volume = hurricane.volume = volumeSlider.value;
 
-  wildfireBtn.classList.remove("playing");
-  hurricaneBtn.classList.remove("playing");
+// Play function
+function playSound(audio, button) {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+    currentButton.classList.remove('playing');
+  }
+
+  audio.play();
+  button.classList.add('playing');
+
+  currentAudio = audio;
+  currentButton = button;
 }
 
-wildfireBtn.addEventListener("click", () => {
-  stopAll();
-  wildfire.play();
-  wildfireBtn.classList.add("playing");
-});
+// Button events
+wildfireBtn.onclick = () => playSound(wildfire, wildfireBtn);
+hurricaneBtn.onclick = () => playSound(hurricane, hurricaneBtn);
 
-hurricaneBtn.addEventListener("click", () => {
-  stopAll();
-  hurricane.play();
-  hurricaneBtn.classList.add("playing");
-});
+// Stop
+stopBtn.onclick = () => {
+  if (!currentAudio) return;
+
+  currentAudio.pause();
+  currentAudio.currentTime = 0;
+  currentButton.classList.remove('playing');
+
+  currentAudio = null;
+  currentButton = null;
+};
+
+// Volume control
+volumeSlider.oninput = () => {
+  wildfire.volume = hurricane.volume = volumeSlider.value;
+};
